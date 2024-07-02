29fe954c273471845c1ca029b0de6d8e035d67d8
Fazlı Altun <altunfazl@gmail.com>
2024-07-02 13:43:35 +0300

    Initial Commit

---
+++ MVVM Pattern Binding with Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Pattern Binding with Movie App
+//
+//  Created by Fazlı Altun on 2.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
