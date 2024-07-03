81474fc80249f9daf854b4bbb259bec6940fcd7b
Fazlı Altun <altunfazl@gmail.com>
2024-07-03 11:07:04 +0300

    created mainviewmodel

--- MVVM Pattern Binding with Movie App/Controllers/MainViewController/MainViewController.swift
+++ MVVM Pattern Binding with Movie App/Controllers/MainViewController/MainViewController.swift
@@ -9,8 +9,40 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var viewmodel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "MVVM"
+        setupTableView()
     }
+}
+
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewmodel.numberOfSections()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewmodel.numberOfRows(in: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
 }
---
+++ MVVM Pattern Binding with Movie App/ViewModel/MainViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewModel.swift
+//  MVVM Pattern Binding with Movie App
+//
+//  Created by Fazlı Altun on 3.07.2024.
+//
+
+import Foundation
+
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+    
+}
