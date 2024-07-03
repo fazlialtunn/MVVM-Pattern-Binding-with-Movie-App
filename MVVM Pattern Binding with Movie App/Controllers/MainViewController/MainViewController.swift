7b4eb2bce79f79bee43ac22223e1d570db9024d9
Fazlı Altun <altunfazl@gmail.com>
2024-07-03 09:40:40 +0300

    project setup and initial changes

---
+++ MVVM Pattern Binding with Movie App/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  MVVM Pattern Binding with Movie App
+//
+//  Created by Fazlı Altun on 3.07.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "MVVM"
+    }
+}
